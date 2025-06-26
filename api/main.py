6d5010830f800df8d4b7f13aa77f5930d86d0a37
lianrag <lianakhangkien@gmail.com>
2025-06-26 14:16:28 +0700

    Update main.py

--- api/main.py
+++ api/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import json, os
 from datetime import datetime
-from langchain.chat_models import ChatGoogleGenerativeAI
+from langchain_community.chat_models import ChatGoogleGenerativeAI
 from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 from langchain.embeddings import GoogleGenerativeAIEmbeddings
